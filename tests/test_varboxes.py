c014c4f9ce4adce3a54d2a095e7a894dc492cbfe
Imam Usmani <imam@e.email>
2023-11-01 20:43:27 +0100

    def test

--- tests/test_varboxes.py
+++ tests/test_varboxes.py
@@ -15,9 +15,21 @@ class TestVarBox(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        pass
+        name = 'test'
+        cls.default_parameters = dict(
+                a=1,
+                b=2,
+                c=3,
+                )
+        cls.parameters = Parameters(name, **cls.default_parameters)
 
-    pass
+    def test_get_keys(self):
+        keys = self.parameters.get_keys()
+        self.assertEqual(self.default_parameters.keys(), keys)
+
+    def test_to_dict(self):
+        dic = self.parameters.to_dict()
+        self.assertDictEqual(self.default_parameters, dic)
 
 
 """ script tests """
