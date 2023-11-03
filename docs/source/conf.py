3fca25297a24c078127b68b389c0961e46152541
Imam Usmani <imam@e.email>
2023-11-03 22:00:45 +0100

    conf exenstions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,10 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+        'sphinx_rtd_theme',
+        'sphinx.ext.autodoc',
+        ]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -23,6 +26,5 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
